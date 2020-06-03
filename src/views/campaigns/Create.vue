d8dbb99ab65168d9c31ec4587040005b6cfa8195
Kanna <heddi932@gmail.com>
2020-06-03 17:26:44 +0200

    Page creation campagne

--- src/views/campaigns/Create.vue
+++ src/views/campaigns/Create.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="campagne">
-    <img id="logo" src="/img/DigitallScore-Logoblanc.png">
-    <CreateCampaign v-if="step === 0" @idCampaign="getObjectives($event)" />
-    <div
-      v-for="(objective, index) in objectives.campaigns_objectives"
-      :key="objective.id"
-    >
-      <Objective
-        v-if="index + 1 === step"
-        :id="objective.id"
-        :idObjective="objective.objective.id"
-        :idCampaign="objectives.id"
-        @next="step++"
-      />
+    <div class="container">
+      <img id="logo" src="/img/DigitallScore-Logoblanc.png">
+      <CreateCampaign v-if="step === 0" @idCampaign="getObjectives($event)" />
+      <div v-for="(objective, index) in objectives.campaigns_objectives" :key="objective.id">
+        <Objective
+          v-if="index + 1 === step"
+          :id="objective.id"
+          :idObjective="objective.objective.id"
+          :idCampaign="objectives.id"
+          @next="step++"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -29,12 +28,9 @@ export default {
   },
   setup(props, context) {
     const { Campaigns } = context.root.$FeathersVuex.api;
-    const { $router } = context.root;
     const step = ref(0);
     const objectives = ref({});
-    function back() {
-      $router.back();
-    }
+
     function getObjectives(idCampaign) {
       Campaigns.get(idCampaign).then(data => (objectives.value = data));
       step.value = 1;
@@ -42,25 +38,24 @@ export default {
     return {
       getObjectives,
       objectives,
-      step,
-      back
+      step
     };
   }
 };
 </script>
 <style scoped>
 #campagne {
- background-image: url("/img/campagne2.jpg");
+  background: linear-gradient(rgba(30, 30, 30, 0.666), rgba(30, 30, 30, 0.666)), url("/img/campagne2.jpg");
   height: 100vh;
   width: 100vw;
   background-size: cover;
-  background-position: left;
+  background-position: center;
 }
 #logo{
   position: absolute;
   top: -50px;
   left: 10px;
-   width: 260px;
+  width: 260px;
   height: 260px;
 }
 
